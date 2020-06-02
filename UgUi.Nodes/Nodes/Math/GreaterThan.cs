46d8febb55422288711b597f556bb701376bf001
filip komorovsky <filip.komorovsky@gmail.com>
2020-06-02 21:38:38 +0200

    + Equals / LessThan / GreaterThan nodes

---
+++ UgUi.Nodes/Nodes/Math/GreaterThan.cs
@@ -0,0 +1,17 @@
+﻿using Ujeby.UgUi.Nodes.Abstract;
+
+namespace Ujeby.UgUi.Nodes.Math
+{
+	[NodeInfo]
+	[IgnoredProperty(Name = nameof(BinaryOperator<double>.C))]
+	public class GreaterThan : BinaryOperator<double>
+	{
+		[Output(AnchorOnly = true, DisplayName = "")]
+		public bool Output { get; protected set; } = false;
+
+		public override void Execute()
+		{
+			Output = A > B;
+		}
+	}
+}
